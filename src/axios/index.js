4532b4f4673723bbd253affa47a224751615be58
Jeremy <jeremy.rotoni@gmail.com>
2020-04-15 18:40:00 +0800

    Add netlify configuration

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://protected-atoll-89994.herokuapp.com/',
+  baseURL: 'https://is226lovewarriors.netlify.com/',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Max-Age': 3600,
